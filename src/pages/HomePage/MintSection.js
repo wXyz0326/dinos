ac799a27f260bc1afb6aae8f3715bc37ca434caf
mr-new-0509 <mr.new0509@gmail.com>
2022-02-08 03:15:06 +0900

    MintSection-responsive

--- src/pages/HomePage/MintSection.js
+++ src/pages/HomePage/MintSection.js
@@ -5,7 +5,7 @@ import Incrementer from '../../components/Incrementor';
 
 const ColorButton = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(grey[100]),
-  fontSize: 16,
+  fontSize: { xl: 16, lg: 16, md: 16, sm: 14, xs: 8 },
   width: '10%',
   backgroundColor: grey[100],
   '&:hover': {
@@ -15,10 +15,19 @@ const ColorButton = styled(Button)(({ theme }) => ({
 
 export default function MintSection() {
   return (
-    <Stack maxWidth="lg" mx="auto" spacing={4} mt={20}>
-      <Typography color={grey[100]} fontSize={56} fontWeight={900} textTransform="uppercase" textAlign="center">
-        Mint
-      </Typography>
+    <Stack
+      maxWidth="lg"
+      mx="auto"
+      spacing={{ xl: 4, lg: 4, md: 4, sm: 2, xs: 2 }}
+      mt={{ xl: 10, lg: 10, md: 10, sm: 5, xs: 3 }}
+    >
+      <Typography
+        color={grey[100]}
+        fontSize={{ xl: 56, lg: 56, md: 48, sm: 32, xs: 24 }}
+        fontWeight={900}
+        textTransform="uppercase"
+        textAlign="center"
+      >Mint</Typography>
       <Stack direction="row" justifyContent="center" alignItems="center" spacing={5}>
         <Incrementer available={10} />
         <ColorButton>Mint</ColorButton>
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -16,7 +16,7 @@ export default function HomePage() {
     <>
       <TopNavbar />
       <HeroSection />
-      {/* <MintSection /> */}
+      <MintSection />
       {/* <LegendSection /> */}
       {/* <PopularSection /> */}
       {/* <DaoSection /> */}
