47006b361ba881f2013f6702498b84bef6e5a165
mr-new-0509 <mr.new0509@gmail.com>
2022-03-01 11:21:35 +0900

    IPhoneDao

--- src/pages/Dao/index.js
+++ src/pages/Dao/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MHidden from '../../components/@mui-extend/MHidden';
 import DesktopDao from './DesktopDao';
 import IPadDao from './IPadDao';
+import IPhoneDao from './IPhoneDao';
 
 export default function Dao() {
   return (
@@ -10,7 +11,12 @@ export default function Dao() {
         <DesktopDao />
       </MHidden>
       <MHidden width="mdUp">
-        <IPadDao />
+        <MHidden width="smDown">
+          <IPadDao />
+        </MHidden>
+        <MHidden width="smUp">
+          <IPhoneDao />
+        </MHidden>
       </MHidden>
     </>
   );
